f3ca5779e76c9b9644d3997e69252fda2419240e
KOBAYASHI <1731237095@qq.com>
2026-05-29 20:24:22 +0800

    2026-05-29-20-24-18

--- khinsider-downloader/src/config.py
+++ khinsider-downloader/src/config.py
@@ -10,4 +10,4 @@ class Config:
     download_delay: float = 1.0
     page_delay: float = 2.0
     download_booklet: bool = True
-    user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
